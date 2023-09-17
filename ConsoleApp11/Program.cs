6d2ce8877db2542ea4a5f3d9879f98ee82e0fb0c
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-18 00:03:22 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+
+            int temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+
+            Console.WriteLine($"Теперь первое число равно {firstNumber}, а второе равно {secondNumber}.");
+        }
+    }
+}
